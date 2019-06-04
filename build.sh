f2607b72fc77d579daa39a0256a19831e64c95ea
michal.kudlacak <kudlacak1@gmail.com>
2019-06-04 03:07:08 +0200

    Added stripping symbols

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 cd ..
 mkdir -p build
 cd build 
-cmake -DCMAKE_BUILD_TYPE=ReleaseWithDebInfo ../CppPhoenixChannels
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../CppPhoenixChannels
 make -j8
+objcopy --only-keep-debug CppPhoenixChannels CppPhoenixChannels.dbg
+objcopy --strip-debug CppPhoenixChannels
 cd ../CppPhoenixChannels
